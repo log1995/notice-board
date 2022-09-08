9a99aa9b01b9a1672b21a85a6cf15739ca44ed85
이원기 <log19951@gmail.com>
2022-09-08 15:24:28 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술 : 
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/test/java/com/log1995/noticeboard/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log1995.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
